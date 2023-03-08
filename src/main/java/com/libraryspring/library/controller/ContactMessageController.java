f1c54b55a7a45347f82b5fb0cde76d136151b1f6
Altug Heper <altugheper@gmail.com>
2023-03-08 23:45:05 +0100

    08/03/23v5

--- src/main/java/com/libraryspring/library/controller/ContactMessageController.java
+++ src/main/java/com/libraryspring/library/controller/ContactMessageController.java
@@ -1,5 +1,6 @@
 package com.libraryspring.library.controller;
 
+import com.fasterxml.jackson.core.json.PackageVersion;
 import com.libraryspring.library.domain.ContactMessage;
 import com.libraryspring.library.dto.ContactMessageDTO;
 import com.libraryspring.library.dto.request.ContactMessageRequest;
@@ -7,6 +8,10 @@ import com.libraryspring.library.dto.response.LbResponse;
 import com.libraryspring.library.mapper.ContactMessageMapper;
 import com.libraryspring.library.repository.ContactMessageRepository;
 import com.libraryspring.library.service.ContactMessageService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +31,8 @@ public class ContactMessageController {
         this.contactMessageService = contactMessageService;
         this.contactMessageMapper = contactMessageMapper;
     }
+
+
 
     // Create Contact Message
     @PostMapping("/visitors")
@@ -49,7 +56,43 @@ public class ContactMessageController {
         List<ContactMessageDTO> contactMessageDTOList = contactMessageMapper.mapDto(contactMessageList);
 
         return ResponseEntity.ok(contactMessageDTOList);
+    }
 
+    // Pageable Get All Contact Message
+    @GetMapping("/pages")
+    public ResponseEntity<Page<ContactMessageDTO>> getAllContactMessageWithPage(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sort") String prop,
+            @RequestParam(value = "direction",
+                            required = false,
+                            defaultValue = "DESC")Sort.Direction direction){
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, prop));
+        Page<ContactMessage> contactMessagePage = contactMessageService.getAllPage(pageable);
+        Page<ContactMessageDTO> pageDTO = getPageDTO(contactMessagePage);
+
+        return ResponseEntity.ok(pageDTO);
+    }
+
+    // Get Contact Message With Path
+    @GetMapping("{id}")
+    public ResponseEntity<ContactMessageDTO> getContactMessageWithPath(@PathVariable("id")Long id){
+
+        ContactMessage contactMessage = contactMessageService.getContactMessage(id);
+
+        ContactMessageDTO contactMessageDTO = contactMessageMapper.contactMessageToDTO(contactMessage);
+
+        return ResponseEntity.ok(contactMessageDTO);
+
+    }
+
+
+
+
+    private Page<ContactMessageDTO> getPageDTO(Page<ContactMessage> contactMessagePage){
+        return contactMessagePage.map(
+                contactMessage -> contactMessageMapper.contactMessageToDTO(contactMessage));
     }
 
 
--- src/main/java/com/libraryspring/library/service/ContactMessageService.java
+++ src/main/java/com/libraryspring/library/service/ContactMessageService.java
@@ -2,6 +2,8 @@ package com.libraryspring.library.service;
 
 import com.libraryspring.library.domain.ContactMessage;
 import com.libraryspring.library.repository.ContactMessageRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +17,8 @@ public class ContactMessageService {
         this.contactMessageRepository = contactMessageRepository;
     }
 
+
+
     public void saveMessage(ContactMessage contactMessage) {
 
         contactMessageRepository.save(contactMessage);
@@ -23,7 +27,21 @@ public class ContactMessageService {
 
     public List<ContactMessage> getAll() {
 
+        return contactMessageRepository.findAll();
 
+    }
+
+
+    public Page<ContactMessage> getAllPage(Pageable pageable) {
+
+        return contactMessageRepository.findAll(pageable);
+
+    }
+
+    public ContactMessage getContactMessage(Long id) {
+
+        return contactMessageRepository.findById(id).orElseThrow(() ->
+                new )
 
     }
 }
